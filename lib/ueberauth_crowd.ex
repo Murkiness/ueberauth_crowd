0457a227f1a553e621ba95e3ee89e6952f652f0c
Владимир Хохренко <v.khokhrenko@fun-box.ru>
2020-10-26 10:28:34 +0300

    initial commit

---
+++ lib/ueberauth_crowd.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthCrowd do
+  @moduledoc """
+  Documentation for `UeberauthCrowd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthCrowd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_crowd_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthCrowdTest do
+  use ExUnit.Case
+  doctest UeberauthCrowd
+
+  test "greets the world" do
+    assert UeberauthCrowd.hello() == :world
+  end
+end
