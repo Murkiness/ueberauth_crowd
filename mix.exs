9314dfa24179d0c13fef407038e77c71c68ca853
Владимир Хохренко <v.khokhrenko@fun-box.ru>
2021-12-03 10:50:16 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule UeberauthCrowd.MixProject do
     [
       app: :ueberauth_crowd,
       description: "Crowd OpenID Strategy for Überauth.",
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
